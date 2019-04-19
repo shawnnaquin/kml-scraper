5d5ce1bebc00b310b3094cebb0b9b11d16e51f5d
snaquin@themorangroup.net <snaquin@themorangroup.net>
2019-04-19 15:05:45 -0500

    simplify even more

--- scripts/simplify-county.sh
+++ scripts/simplify-county.sh
@@ -5,5 +5,5 @@ mkdir dist/simplified/county/
 for f in dist/county/*.kml ; do FILENAME=`basename ${f%%}`;
     echo ${f};
     echo ${FILENAME};
-    ogr2ogr -f libkml -simplify 0.001 dist/simplified/county/${FILENAME} dist/county/${FILENAME}
+    ogr2ogr -f libkml -simplify 0.01 dist/simplified/county/${FILENAME} dist/county/${FILENAME}
 done;
--- scripts/simplify-zip.sh
+++ scripts/simplify-zip.sh
@@ -5,5 +5,5 @@ mkdir dist/simplified/zip
 for f in dist/zip/*.kml ; do FILENAME=`basename ${f%%}`;
     echo ${f};
     echo ${FILENAME};
-    ogr2ogr -f libkml -simplify 0.001 dist/simplified/zip/${FILENAME} dist/zip/${FILENAME}
+    ogr2ogr -f libkml -simplify 0.01 dist/simplified/zip/${FILENAME} dist/zip/${FILENAME}
 done;

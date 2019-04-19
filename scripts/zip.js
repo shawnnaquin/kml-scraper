b02ce4075b41d2fbf2d965895db5df28b3b06364
snaquin@themorangroup.net <snaquin@themorangroup.net>
2019-04-19 11:06:06 -0500

    finished zips

--- scripts/zip.js
+++ scripts/zip.js
@@ -17,71 +17,58 @@ xmlReader.readXML( fse.readFileSync( FILE ), (err, data) => {
 
     let xml = data.content;
     let result = JSON.parse( convert.xml2json( xml, { compact: true, spaces: 4 } ) );
-    // let places = result.kml.Document.Folder.Placemark;
+    let places = result.kml.Document.Folder.Placemark;
 
-    console.log( result );
-    //
-    // Object.keys( places ).forEach( ( place, placeIndex ) => {
-    //
-    //     let $item = places[place];
-    //     // let $item = places[0];
-    //     let normalizedStateName;
-    //
-    //     let rawName = $item.name._text.split(' ').join('_');
-    //     let parsedName = rawName.split('<at><openparen>')[1].split('<closeparen>')[0];
-    //     let lowerCaseName = parsedName.toLocaleLowerCase();
-    //     let normalizedName = lowerCaseName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    //
-    //     for ( let $extra of $item.ExtendedData.SchemaData.SimpleData ) {
-    //
-    //         if ( $extra._attributes.name === 'STATEFP' ) {
-    //             normalizedStateName = statefips[$extra._text].replace(' ', '_').toLocaleLowerCase();
-    //         }
-    //
-    //     }
-    //
-    //     let newResult = result;
-    //
-    //     delete newResult.kml.Document.Folder;
-    //
-    //     newResult.kml.Document.name = 'Counties';
-    //
-    //     $item._attributes.id = `${ normalizedStateName}/${normalizedName}`;
-    //     $item.name._text = `${ normalizedStateName}/${normalizedName}`;
-    //
-    //     newResult.kml.Document.Folder = {
-    //         name: `${ normalizedStateName}/${normalizedName}`,
-    //         Placemark: [ $item ]
-    //     };
-    //
-    //     let newXML = xml;
-    //
-    //     newXML = convert.json2xml( newResult, {compact: true, ignoreComment: true, spaces: 0 } );
-    //
-    //     const FOLDER = path.join( DIST );
-    //
-    //     fse.ensureDir( FOLDER )
-    //         .then(() => {
-    //             const FILE = path.join( FOLDER, `/${normalizedName}.kml`);
-    //             fse.ensureFile( FILE )
-    //                 .then(() => {
-    //                     fse.writeFile( FILE, newXML, function(err, data) {
-    //                         if (err) {
-    //                             console.log(err);
-    //                         }
-    //                         else {
-    //                             // console.log('updated!');
-    //                         }
-    //                     });
-    //                 })
-    //                 .catch(err => {
-    //                     console.error(err)
-    //                 })
-    //         })
-    //         .catch(err => {
-    //             console.error(err)
-    //         });
-    //
-    // });
+    Object.keys( places ).forEach( ( place, placeIndex ) => {
+        let $item = places[place];
+        // let $item = places[0];
+        // console.log( $item );
+        let normalizedStateName;
+        let rawName = $item.name._text.split(' ').join('_');
+        let parsedName = rawName.split('<at><openparen>')[1].split('<closeparen>')[0];
+        let lowerCaseName = parsedName.toLocaleLowerCase();
+        let normalizedName = lowerCaseName.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+
+        let newResult = result;
+
+        delete newResult.kml.Document.Folder;
+
+        newResult.kml.Document.name = 'ZCTA';
+
+        $item._attributes.id = `${normalizedName}`;
+        $item.name._text = `${normalizedName}`;
+
+        newResult.kml.Document.Folder = {
+            name: `${ normalizedStateName}/${normalizedName}`,
+            Placemark: [ $item ]
+        };
+        let newXML = xml;
+        newXML = convert.json2xml( newResult, {compact: true, ignoreComment: true, spaces: 0 } );
+
+        const FOLDER = path.join( DIST );
+
+        fse.ensureDir( FOLDER )
+            .then(() => {
+                const FILE = path.join( FOLDER, `/${normalizedName}.kml`);
+                fse.ensureFile( FILE )
+                    .then(() => {
+                        fse.writeFile( FILE, newXML, function(err, data) {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else {
+                                // console.log('updated!');
+                            }
+                        });
+                    })
+                    .catch(err => {
+                        console.error(err)
+                    })
+            })
+            .catch(err => {
+                console.error(err)
+            });
+
+    });
 
 });

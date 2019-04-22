aa8b115081c5e31c7c51966dc4888a2d6ec929c9
shawnnaquin <shawn.naquin@gmail.com>
2019-04-22 13:33:08 -0500

    extra precision

--- scripts/simplify.sh
+++ scripts/simplify.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+PRECISION=0.001;
 TYPE=$1;
 SIMPLE=dist/simplified/$TYPE;
 COMPLEX=dist/complex/$TYPE;
@@ -11,7 +11,7 @@ for f in ${COMPLEX}/*.kml ; do FILENAME=`basename ${f} .kml`;
     FILE=${SIMPLE}/${FILENAME};
     echo $FILENAME;
 
-    ogr2ogr -f libkml -simplify 0.01 ${SIMPLE}/${FILENAME}.kml ${COMPLEX}/${FILENAME}.kml;
+    ogr2ogr -f libkml -simplify $PRECISION ${SIMPLE}/${FILENAME}.kml ${COMPLEX}/${FILENAME}.kml;
     ogr2ogr -f GeoJSON ${SIMPLE}/${FILENAME}.json ${SIMPLE}/${FILENAME}.kml
     rm ${SIMPLE}/${FILENAME}.kml;
 
